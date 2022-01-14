e6bdf6737349b3a07c99734e5e6d4b610a9719e8
justus7 <justuskasyoki57@gmail.com>
2022-01-14 11:02:55 +0300

    committed

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -31,7 +31,10 @@ export class TaskService {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.put<Task>(url, task, httpOptions);
   }
-
+  
+  AddTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
   
 
 }
